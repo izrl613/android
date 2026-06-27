daaf473a289de3dfa6156229b50f003c67507482
Israel David <agape@sovereign.nyc>
2026-06-27 18:03:08 -0400

    chore: configure ESLint for cloud functions and add linting to firebase deployment pipeline

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -29,6 +29,5 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
-    "max-len": ["error", {"code": 120, "ignoreUrls": true, "ignoreStrings": true, "ignoreTemplateLiterals": true}],
   },
 };
